7351bc2b37ae991cbbd108090d859540f99b8cd1
Marc Guinea <develop.marcguinea@gmail.com>
2025-02-24 07:36:15 +0100

    fix: remove unused default classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommentsModule } from './comments/infrastructure/nestjs/comments.module';
 
 @Module({
   imports: [CommentsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
